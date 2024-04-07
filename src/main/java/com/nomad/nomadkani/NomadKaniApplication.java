7275ea0dba15846358e4edd2c631201daa235c98
Kaniet Kydyrbekova <kydyrbekova995@gmail.com>
2024-04-07 18:05:01 +0200

    first commit

---
+++ src/main/java/com/nomad/nomadkani/NomadKaniApplication.java
@@ -0,0 +1,13 @@
+package com.nomad.nomadkani;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NomadKaniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NomadKaniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nomad/nomadkani/NomadKaniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nomad.nomadkani;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NomadKaniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
